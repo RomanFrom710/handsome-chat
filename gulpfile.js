264947284a747ec3062fe0282283a75858bc71fb
Roman Chaley <romanchaley@gmail.com>
2016-02-21 14:21:09 +0300

    Added bower, npm and gulp files

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+var gulp = require('gulp');
+
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var less = require('gulp-less');
+var cssNano = require('gulp-cssnano');
+var nodemon = require('gulp-nodemon');
+var autoprefixer = require('gulp-autoprefixer');
+var templateCache = require('gulp-angular-templatecache');
+var plumber = require('gulp-plumber');
+
+var environments = require('gulp-environments');
+var production = environments.production;
+
+var compiledPath = 'public/compiled';
+
+var vendors = [
+    'angular/angular.js',
+    'angular-ui-router/release/angular-ui-router.js',
+    'restangular/dist/restangular.js',
+    'lodash/lodash.js'
+];
+vendors = vendors.map(function (path) { return 'bower_components/' + path; });
+
+var scripts = [
+    'app.js',
+    '**/module.js',
+    '**/*.js'
+];
+scripts = scripts.map(function (path) { return 'src/client/app/' + path; });
+
+
+gulp.task('vendor-scripts', function() {
+    return gulp.src(vendors)
+        .pipe(concat('vendor.js'))
+        .pipe(production(uglify()))
+        .pipe(gulp.dest(compiledPath));
+});
+
+
+gulp.task('scripts', function() {
+    return gulp.src(scripts)
+        .pipe(concat('app.js'))
+        .pipe(production(uglify()))
+        .pipe(gulp.dest(compiledPath));
+});
+
+
+gulp.task('less', function() {
+    return gulp.src('src/client/styles/main.less')
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(concat('styles.css'))
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(production(cssNano()))
+        .pipe(gulp.dest(compiledPath));
+});
+
+
+gulp.task('templates', function() {
+    return gulp.src('src/**/*.html')
+       .pipe(templateCache('templates.js', { module: 'templates' }))
+       .pipe(gulp.dest(compiledPath));
+});
+
+
+gulp.task('watch', function () {
+    gulp.watch('src/client/styles/**/*.less', ['less']);
+    gulp.watch('src/client/app/**/*.js', ['scripts']);
+    gulp.watch('src/client/**/*.html', ['templates']);
+});
+
+
+gulp.task('start', function () {
+    var env = production() ? 'production' : 'development';
+    nodemon({
+        script: 'server.js',
+        env: { 'NODE_ENV': env }
+    });
+});
+
+
+gulp.task('default', ['vendor-scripts', 'scripts', 'templates', 'less', 'watch', 'start']);
