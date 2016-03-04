1ffbd0690207b80cf941c9152c0cfbdd3e400359
Roman Chaley <romanchaley@gmail.com>
2016-03-04 21:14:00 +0300

    Fixed issue with mongoose connection fail

--- src/server/configs/local.js
+++ src/server/configs/local.js
@@ -1,5 +1,5 @@
 var config = {
-    mongoConnectionString: 'mongodb://localhost/handsomechat'
+    mongoConnectionString: 'mongodb://127.0.0.1/handsomechat'
 };
 //todo: add client urls
 module.exports = config;
