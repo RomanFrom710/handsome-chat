39df7e77878acd8f0a113e1056491196f99d29fb
Roman Chaley <romanchaley@gmail.com>
2016-03-08 01:13:00 +0300

    Fixing updating current user

--- src/client/app/common/mainController.js
+++ src/client/app/common/mainController.js
@@ -8,8 +8,8 @@
     function mainController($state, userService) {
         var vm = this;
 
-        vm.currentUser = userService.getCurrentUser();
-        if (vm.currentUser) {
+        vm.getCurrentUser = userService.getCurrentUser;
+        if (vm.getCurrentUser()) {
             $state.go('chat');
         } else {
             $state.go('login');
