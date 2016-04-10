bd4085a153a2b9acbb7aa55016f006330afc75d1
Roman Chaley <romanchaley@gmail.com>
2016-04-10 19:04:41 +0300

    Removed room references

--- src/server/chat/message/message-model.js
+++ src/server/chat/message/message-model.js
@@ -6,7 +6,6 @@ var Schema = mongoose.Schema;
 var messageSchema = new Schema({
     content: String,
     created: { type: Date, default: Date },
-    room: { type: Schema.Types.ObjectId, ref: 'Room' },
     author: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
